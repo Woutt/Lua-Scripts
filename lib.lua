9bf913f0460718732b4e849b6c4a18080f28ac9f
Fludex <71903277+Woutt@users.noreply.github.com>
2022-04-15 13:20:00 +0200

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -10,53 +10,53 @@
     local HTTPService = game:GetService("HttpService")
 
     local Library = {
-        Themes = readfile("theme.txt"),--{
-            -- Legacy = {--idk
-            --     Main = Color3.fromHSV(262/360, 60/255, 34/255),
-            --     Secondary = Color3.fromHSV(240/360, 40/255, 63/255),
-            --     Tertiary = Color3.fromHSV(260/360, 60/255, 148/255),
-
-            --     StrongText = Color3.fromHSV(0, 0, 1),		
-            --     WeakText = Color3.fromHSV(0, 0, 172/255)
-            -- },
-            -- Serika = {--idk
-            --     Main = Color3.fromRGB(50, 52, 55),
-            --     Secondary = Color3.fromRGB(80, 82, 85),
-            --     Tertiary = Color3.fromRGB(226, 183, 20),
-
-            --     StrongText = Color3.fromHSV(0, 0, 1),		
-            --     WeakText = Color3.fromHSV(0, 0, 172/255)
-            -- },
-            -- Dark = {
-            --     Main = Color3.fromRGB(30, 30, 35),
-            --     Secondary = Color3.fromRGB(50, 50, 55),
-            --     Tertiary = Color3.fromRGB(70, 130, 180),
-
-            --     StrongText = Color3.fromHSV(0, 0, 1),		
-            --     WeakText = Color3.fromHSV(0, 0, 172/255)
-            -- },
-            -- Rust = {--change color
-            --     Main = Color3.fromRGB(37, 35, 33),
-            --     Secondary = Color3.fromRGB(65, 63, 63),
-            --     Tertiary = Color3.fromRGB(237, 94, 38),
-
-            --     StrongText = Color3.fromHSV(0, 0, 1),		
-            --     WeakText = Color3.fromHSV(0, 0, 172/255)
-            -- },
-            -- Aqua = {--change color
-            --     Main = Color3.fromRGB(30, 30, 35),
-            --     Secondary = Color3.fromRGB(60, 60, 60),
-            --     Tertiary = Color3.fromRGB(70, 130, 180),
-
-            --     StrongText = Color3.fromHSV(0, 0, 1),		
-            --     WeakText = Color3.fromHSV(0, 0, 172/255)
-            -- },
-        --     Vaporwave = {},
-        --     OperaGX = {},
-        --     VisualStudio = {}
-        -- },
+        Themes = {
+            Legacy = {
+                Main = Color3.fromHSV(262/360, 60/255, 34/255),
+                Secondary = Color3.fromHSV(240/360, 40/255, 63/255),
+                Tertiary = Color3.fromHSV(260/360, 60/255, 148/255),
+    
+                StrongText = Color3.fromHSV(0, 0, 1),		
+                WeakText = Color3.fromHSV(0, 0, 172/255)
+            },
+            Serika = {
+                Main = Color3.fromRGB(50, 52, 55),
+                Secondary = Color3.fromRGB(80, 82, 85),
+                Tertiary = Color3.fromRGB(226, 183, 20),
+    
+                StrongText = Color3.fromHSV(0, 0, 1),		
+                WeakText = Color3.fromHSV(0, 0, 172/255)
+            },
+            Dark = {
+                Main = Color3.fromRGB(30, 30, 35),
+                Secondary = Color3.fromRGB(50, 50, 55),
+                Tertiary = Color3.fromRGB(70, 130, 180),
+    
+                StrongText = Color3.fromHSV(0, 0, 1),		
+                WeakText = Color3.fromHSV(0, 0, 172/255)
+            },
+            Rust = {
+                Main = Color3.fromRGB(37, 35, 33),
+                Secondary = Color3.fromRGB(65, 63, 63),
+                Tertiary = Color3.fromRGB(237, 94, 38),
+    
+                StrongText = Color3.fromHSV(0, 0, 1),		
+                WeakText = Color3.fromHSV(0, 0, 172/255)
+            },
+            Aqua = {
+                Main = Color3.fromRGB(19, 21, 21),
+                Secondary = Color3.fromRGB(65, 63, 63),
+                Tertiary = Color3.fromRGB(51, 153, 137),
+    
+                StrongText = Color3.fromHSV(0, 0, 1),        
+                WeakText = Color3.fromHSV(0, 0, 172/255)
+            },
+            Vaporwave = {},
+            OperaGX = {},
+            VisualStudio = {}
+        },
         ColorPickerStyles = {
-            Legacy = 0,
+            Legacy = 1,--0
             Modern = 1
         },
         Toggled = true,
